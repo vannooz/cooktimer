cd08b0b1bf6840b2d731ae2d36c777a829bc48d0
vannooz <vannooz4@gmail.com>
2016-02-07 20:18:18 -0800

    swtich to swift

---
+++ cooktimer/cooktimerTests/cooktimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  cooktimerTests.swift
+//  cooktimerTests
+//
+//  Created by Juneja, Vanuj on 2/7/16.
+//  Copyright © 2016 Juneja, Vanuj. All rights reserved.
+//
+
+import XCTest
+@testable import cooktimer
+
+class cooktimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
